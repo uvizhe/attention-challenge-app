9e59162e4d8d6f084745c235ef366513580f34a5
Alexander Uvizhev <uvizhe@yandex.ru>
2020-04-27 21:35:37 +0300

    remove quasar logo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,9 +4,6 @@
     <q-header reveal class="bg-primary text-white">
       <q-toolbar>
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>
           Attention Challenge
         </q-toolbar-title>
         <toolbar-buttons />
