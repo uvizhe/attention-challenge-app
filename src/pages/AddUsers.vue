4afb05da5b2b9d168e65615f56ade97a1d4db436
Alexander Uvizhev <uvizhe@yandex.ru>
2020-05-09 19:51:04 +0300

    limit user selection to 4 items

--- src/pages/AddUsers.vue
+++ src/pages/AddUsers.vue
@@ -1,5 +1,6 @@
 <template>
   <q-page padding>
+    <q-banner :class="noticeBannerClass">You can select only 4 users</q-banner>
     <q-list dense>
       <q-item :inset-level="1" class="text-bold">
         <q-item-section>
@@ -9,9 +10,9 @@
           Score
         </q-item-section>
       </q-item>
-      <q-item v-for="user in users" :key="user.id" tag="label">
+      <q-item v-for="(user, idx) in users" :key="user.id" tag="label">
         <q-item-section side>
-          <q-checkbox v-model="user.checked" />
+          <q-checkbox v-model="user.checked" @input="checkSelected(idx)" />
         </q-item-section>
         <q-item-section>
           {{ user.user }}
@@ -42,10 +43,31 @@ export default {
   data () {
     return {
       users: [],
-      check1: false
+      limitNotification: false
+    }
+  },
+  computed: {
+    noticeBannerClass: function () {
+      let cls = 'absolute-top z-top bg-warning text-white text-center'
+      if (!this.limitNotification) {
+        cls += ' hidden'
+      }
+      return cls
     }
   },
   methods: {
+    showNotification () {
+      this.limitNotification = true
+      setTimeout(() => {
+        this.limitNotification = false
+      }, 3000)
+    },
+    checkSelected (idx) {
+      if (this.users.filter(i => i.checked).length > 4) {
+        this.users[idx].checked = !this.users[idx].checked
+        this.showNotification()
+      }
+    },
     prepareUserList (users) {
       const friends = this.$store.state.app.totalsChartFriends
       return users.map(i => {
