45850bfe2fc3f01a6d15a23e72c48ae3130db378
Alexander Uvizhev <uvizhe@yandex.ru>
2020-09-11 18:06:28 +0300

    produce eventlog from sessions

--- src/components/EventLog.vue
+++ src/components/EventLog.vue
@@ -5,14 +5,14 @@
     <div class="fixed-top gradient-2"></div>
     <div class="eventlog overflow-hidden-y">
       <q-list class="">
-        <q-item v-for="(event, idx) in eventlog"
+        <q-item v-for="event in eventlog"
           :key="event.id"
           class="event"
         >
           <q-item-section side class="eventlog-date">
             <div class="fit">
               <span>{{ event.date }}</span>
-              <span v-if="idx >= eventlog.length - 2" class="float-right text-red">*</span>
+              <span v-if="eventlog.new" class="float-right text-red">*</span>
             </div>
           </q-item-section>
           <q-item-section>
@@ -22,10 +22,10 @@
               <span>, </span>
               <span class="">{{ event.min }}</span>
               <span> min</span>
-              <span class="float-right" style="width: 8.5em">
-                <span v-if="event.week" class="float-left">Last week: {{ event.week }}</span>
+              <span class="eventlog-week float-right">
+                <span v-if="event.week" class="float-left">This week: {{ event.week }}</span>
               </span>
-              <span v-if="idx === eventlog.length -2" class="q-mr-xs float-right">&#128081;</span>
+              <span v-if="eventlog.king" class="q-mr-xs float-right">&#128081;</span>
             </div>
           </q-item-section>
         </q-item>
@@ -35,27 +35,16 @@
 </template>
 
 <script>
+import { makeEL } from '../js/eventlog'
 export default {
   name: 'EventLog',
   data () {
     return {
-      eventlog: [
-        { date: '', user: 'you', min: 15, week: '' },
-        { date: '', user: 'VasyachandraSuperhero', min: 20, week: '' },
-        { date: '', user: 'Ann', min: 7, week: '' },
-        { date: '', user: 'you', min: 15, week: '' },
-        { date: '', user: 'you', min: 15, week: '' },
-        { date: '', user: 'VasyachandraSuperhero', min: 30, week: '' },
-        { date: '', user: 'VasyachandraSuperhero', min: 20, week: '' },
-        { date: '', user: 'VasyachandraSuperhero', min: 10, week: '' },
-        { date: '', user: 'VasyachandraSuperhero', min: 20, week: '' },
-        { date: 'Earlier↑↑↑', user: 'Ann', min: 7, week: '' },
-        { date: 'Yesterday', user: 'you', min: 15, week: '' },
-        { date: '', user: 'Ann', min: 7, week: '49 min' },
-        { date: '', user: 'you', min: 15, week: '' },
-        { date: 'Today', user: 'VasyachandraSuperhero', min: 30, week: '3+ hr' },
-        { date: '', user: 'you', min: 15, week: '2+ hr' }
-      ]
+    }
+  },
+  computed: {
+    eventlog () {
+      return makeEL(this.$store.state.app.sessions)
     }
   }
 }
---
+++ src/js/eventlog.js
@@ -0,0 +1,99 @@
+import moment from 'moment'
+
+const TD = 'Todate'
+const YD = 'Yesterdate'
+const EL = 'Earlier↑↑↑'
+
+export function makeEL (sessions) {
+  /* Convert sessions:
+      [{
+        date: YYYY-MM-DD,
+        ts: unixtime,
+        duration: minutes
+      }, ...]
+     to eventlog:
+      [{
+        date: 'Todate|Yesterdate|Earlier|_',
+        user: 'you',
+        min: minutes,
+        week: thisWeekTotal|_
+      }, ...]
+  */
+  const eventlog = []
+  const todate = moment().subtract(4, 'hours').format('YYYY-MM-DD')
+  const twoDatesAgo = moment().subtract(2, 'dates').format('YYYY-MM-DD')
+  const weekAgo = moment().subtract(1, 'week').format('YYYY-MM-DD')
+  let weekTotal = 0
+  for (const s of sessions) {
+    const date = calcDate(s.date, todate, twoDatesAgo)
+    if (s.date > weekAgo) {
+      weekTotal += s.duration
+    }
+    eventlog.push({
+      date: date,
+      user: 'you',
+      min: s.duration,
+      week: ''
+    })
+  }
+  const lastId = eventlog.length - 1
+  eventlog[lastId].week = weekTotalString(weekTotal)
+  removeDateDuplicates(eventlog)
+  if (eventlog[lastId].date === EL) {
+    eventlog[lastId].date = sessions[lastId].date
+    eventlog[lastId - 1].date = EL
+  }
+  return eventlog
+}
+
+function calcDate (sessionDate, todate, twoDatesAgo) {
+  let date = YD
+  if (sessionDate === todate) {
+    date = TD
+  } else if (sessionDate <= twoDatesAgo) {
+    date = EL
+  }
+  return date
+}
+
+function weekTotalString (weekTotalMinutes) {
+  let weekTotalString
+  if (weekTotalMinutes >= 60) {
+    const hrs = weekTotalMinutes / 60
+    weekTotalString = hrs.toString()
+    if (hrs > Math.floor(hrs)) {
+      weekTotalString += '+'
+    }
+    weekTotalString += ' hr'
+  } else {
+    weekTotalString = weekTotalMinutes + ' min'
+  }
+  return weekTotalString
+}
+
+function removeDateDuplicates (eventlog) {
+  let isTDFound = false
+  let isYDFound = false
+  for (let i = 0; i < eventlog.length; i++) {
+    const date = eventlog[i].date
+    if (date === TD) {
+      if (!isTDFound) {
+        isTDFound = true
+      } else {
+        eventlog[i].date = ''
+      }
+    } else if (date === YD) {
+      if (!isYDFound) {
+        isYDFound = true
+      } else {
+        eventlog[i].date = ''
+      }
+    } else if (date === EL) {
+      if (i < eventlog.length - 1) {
+        if (eventlog[i + 1].date === EL) {
+          eventlog[i].date = ''
+        }
+      }
+    }
+  }
+}
