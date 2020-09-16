d7219840f3b92c1b30d04f3e0d86e9d521a20cfe
Alexander Uvizhev <uvizhe@yandex.ru>
2020-09-16 15:28:22 +0300

    show score as stars in EventLog component

--- src/components/EventLog.vue
+++ src/components/EventLog.vue
@@ -17,11 +17,15 @@
           </q-item-section>
           <q-item-section>
             <div class="eventlog-data">
-              <span v-if="event.date" class="eventlog-name ellipsis vertical-top">{{ event.user }}</span>
-              <span v-else class="">{{ event.user }}</span>
-              <span>, </span>
               <span class="">{{ event.min }}</span>
               <span> min</span>
+              <span>, </span>
+              <span v-if="event.score === 0" class="text-primary">&star;&star;&star;&star;&star;</span>
+              <span v-else-if="event.score === 1" class="text-primary">&starf;&star;&star;&star;&star;</span>
+              <span v-else-if="event.score === 2" class="text-primary">&starf;&starf;&star;&star;&star;</span>
+              <span v-else-if="event.score === 3" class="text-primary">&starf;&starf;&starf;&star;&star;</span>
+              <span v-else-if="event.score === 4" class="text-primary">&starf;&starf;&starf;&starf;&star;</span>
+              <span v-else-if="event.score === 5" class="text-primary">&starf;&starf;&starf;&starf;&starf;</span>
               <span class="eventlog-week float-right">
                 <span v-if="event.week" class="float-left">This week: {{ event.week }}</span>
               </span>
