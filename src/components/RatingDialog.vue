4befdaf8c99e52ee08f54d31cb83465d9335acee
Alexander Uvizhev <uvizhe@yandex.ru>
2020-10-09 14:04:22 +0300

    amend rating dialog with blue stars and more

--- src/components/RatingDialog.vue
+++ src/components/RatingDialog.vue
@@ -2,20 +2,21 @@
   <div>
   <q-dialog v-model="show" persistent transition-hide="scale">
     <q-card>
-      <q-card-section align="center">
+      <q-card-section align="center" class="rating-dialog-header">
         <div class="text-h6">{{ $t('ratingHeader') }}</div>
       </q-card-section>
       <q-card-section>
         <q-rating
           v-model="score"
           size="3.5em"
-          color="yellow-14"
-          icon="star"
+          color="primary"
+          icon="star_border"
+          icon-selected="star"
           :readonly="wait"
         />
       </q-card-section>
       <q-card-actions align="around">
-        <q-btn flat :label="$t('ratingReset')" color="primary" @click="score=0" :disable="wait" />
+        <q-btn flat :label="$t('ratingReset')" color="primary" @click="score=0" :disable="wait || score === 0" />
         <q-btn flat label="OK" color="primary" @click="reportScore" :disable="wait" />
       </q-card-actions>
       <q-linear-progress indeterminate :class="progressClass" />
